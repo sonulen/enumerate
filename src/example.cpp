4d399cfd16ddbe47ffeb63297661cb43f79feb8d
sonulen <sonulen@gmail.com>
2021-03-29 16:04:42 +0300

    update: Добавлена сборка примеров использования

--- src/example.cpp
+++ src/example.cpp
@@ -4,10 +4,12 @@
 
 using mtl::enumerate;
 
-int main(int argc, const char* argv[]) {
+int main() {
     std::array<double, 10> array{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
     for (const auto& [i, v] : enumerate(array)) {
         std::cout << "Index : " << i << " with value: " << v << std::endl;
     }
+
     return 0;
 }
---
+++ test/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "TESTS!" << std::endl;
+    return 0;
+}
